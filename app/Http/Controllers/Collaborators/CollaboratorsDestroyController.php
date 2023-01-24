bf717f005df69f977bc0f59d5cf2d24375a68ce7
Luizmagao <luizmagao@gmail.com>
2023-01-24 17:22:27 -0300

    :sparkles: Atualizar delete collaborators

--- app/Http/Controllers/Collaborators/CollaboratorsDestroyController.php
+++ app/Http/Controllers/Collaborators/CollaboratorsDestroyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Collaborators;
 
 use App\Http\Controllers\Controller;
 use App\Models\Collaborator;
+use Exception;
 use Illuminate\Http\Request;
 
 class CollaboratorsDestroyController extends Controller
@@ -15,9 +16,12 @@ class CollaboratorsDestroyController extends Controller
 
     public function __invoke($id)
     {
-        $collaborator = $this->collaborator->with(['image', 'email'])->find($id);
-        $collaborator->delete();
-
-        return response()->json([], 204);
+        try {
+            $collaborator = $this->collaborator->with(['image', 'email'])->find($id);
+            $collaborator->delete();
+            return response()->json([], 204);
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
+        }
     }
 }
