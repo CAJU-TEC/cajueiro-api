b0ce80ea4f4e5e7cddd15a7eabc4401102dffb00
Luizmagao <luizmagao@gmail.com>
2023-01-24 10:50:31 -0300

    :sparkles: Atualizar corporate

--- app/Http/Controllers/Corporates/CorporateStoreController.php
+++ app/Http/Controllers/Corporates/CorporateStoreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Corporates;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\API\Corporate\CorporateStoreRequest;
 use App\Models\Corporate;
+use Exception;
 use Illuminate\Http\Request;
 
 class CorporateStoreController extends Controller
@@ -15,39 +16,44 @@ class CorporateStoreController extends Controller
 
     public function __invoke(CorporateStoreRequest $request)
     {
-        $corporate = $this->corporate->create($request->only([
-            'first_name',
-            'last_name',
-            'address'
-        ]));
+        try {
+            $corporate = $this->corporate->create($request->only([
+                'first_name',
+                'last_name',
+                'address'
+            ]));
 
-        if ($request->email) {
-            $corporate->email()->updateOrCreate([
-                'emailable_id' => $corporate->id,
-            ], [
-                'description' => $request->email
-            ]);
+            if ($request->email) {
+                $corporate->email()->updateOrCreate([
+                    'emailable_id' => $corporate->id,
+                ], [
+                    'description' => $request->email
+                ]);
+            }
+
+            if ($request->image) {
+                $name = $corporate->id . '.' . explode(
+                    '/',
+                    explode(
+                        ':',
+                        substr(
+                            $request->image,
+                            0,
+                            strpos($request->image, ';')
+                        )
+                    )[1]
+                )[1];
+                $uri = storage_path('app/public/images/') . $name;
+                \Image::make($request->image)->save($uri);
+
+                $corporate->image()->updateOrCreate([
+                    'uri' => $name
+                ]);
+            }
+        } catch (Exception $e) {
+            return response()->json($e->getMessage(), 500);
         }
 
-        if ($request->image) {
-            $name = $corporate->id . '.' . explode(
-                '/',
-                explode(
-                    ':',
-                    substr(
-                        $request->image,
-                        0,
-                        strpos($request->image, ';')
-                    )
-                )[1]
-            )[1];
-            $uri = storage_path('app/public/images/') . $name;
-            \Image::make($request->image)->save($uri);
-
-            $corporate->image()->updateOrCreate([
-                'uri' => $name
-            ]);
-        }
 
         return response()->json($corporate, 201);
     }
