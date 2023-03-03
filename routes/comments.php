a0f4ac398833564f669057ef837a5a7779dd633e
Luizmagao <luizmagao@gmail.com>
2023-03-03 11:47:26 -0300

    :art: removendo acl comments

--- routes/comments.php
+++ routes/comments.php
@@ -7,6 +7,6 @@ use Illuminate\Support\Facades\Route;
 // corporates
 Route::get('comments', CommentsIndexController::class)->name('index')->middleware(['role_or_permission:super-admin|comments.index']);
 // Route::get('corporates/{corporate}', CorporateShowController::class)->name('show');
-Route::post('comments', CommentsStoreController::class)->name('store')->middleware(['role_or_permission:super-admin|comments.store']);
+Route::post('comments', CommentsStoreController::class)->name('store');
 // Route::put('corporates/{corporate}', CorporateUpdateController::class)->name('update');
 // Route::delete('corporates/{corporate}', CorporateDestroyController::class)->name('destroy');
