8c2284d52fdd67d8bc564144839956cf4e0be2c2
Luizmagao <luizmagao@gmail.com>
2023-03-03 11:38:18 -0300

    :art: mostrar o rosto do responsavel

--- app/Http/Controllers/Tickets/TicketsShowController.php
+++ app/Http/Controllers/Tickets/TicketsShowController.php
@@ -18,7 +18,7 @@ class TicketsShowController extends Controller
             'impact',
             'client.email',
             'image',
-            'user.collaborator',
+            'user.collaborator.image',
             'collaborator.image',
             'comments.collaborator.image',
             'comments.image',
