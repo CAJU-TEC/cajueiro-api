9ac7ccd8ff48527c9686f1c313509bc17c382f2f
Luizmagao <luizmagao@gmail.com>
2023-03-07 16:50:28 -0300

    :art: atualizando report

--- app/Http/Controllers/Tickets/TicketsKanbanController.php
+++ app/Http/Controllers/Tickets/TicketsKanbanController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Tickets;
 use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Http\Request;
-use PDF;
+// use PDF;
 
 class TicketsKanbanController extends Controller
 {
@@ -19,22 +19,22 @@ class TicketsKanbanController extends Controller
 
     public function __construct()
     {
-        $this->pdf = PDF::loadHTML('');
+        // $this->pdf = PDF::loadHTML('');
     }
 
     public function __invoke(Request $request)
     {
         //
-        try {
-            $this->pdf->loadView('reports.kanban');
-            $this->pdf->setOptions([
-                'page-width' => '6.5in',
-                'disable-smart-shrinking' => true,
-                'page-size' => 'a6'
-            ]);
-            return $this->pdf->stream();
-        } catch (Exception $e) {
-            return response()->json($e->getMessage());
-        }
+        // try {
+        //     $this->pdf->loadView('reports.kanban');
+        //     $this->pdf->setOptions([
+        //         'page-width' => '6.5in',
+        //         'disable-smart-shrinking' => true,
+        //         'page-size' => 'a6'
+        //     ]);
+        //     return $this->pdf->stream();
+        // } catch (Exception $e) {
+        //     return response()->json($e->getMessage());
+        // }
     }
 }
