c71b532778894f88208315e604a047caab5b9b2a
Luizmagao <luizmagao@gmail.com>
2023-03-24 08:36:21 -0300

    :art: liberando user

--- routes/users.php
+++ routes/users.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 // users
 Route::get('users', AuthIndexController::class)->name('index');
 Route::get('users/{user}', AuthShowController::class)->name('show');
-Route::put('users/{user}', AuthUpdateController::class)->name('update')->middleware(['role_or_permission:super-admin|users.*']);
+Route::put('users/{user}', AuthUpdateController::class)->name('update');
 Route::post('users', AuthStoreController::class)->name('store')->middleware(['role_or_permission:super-admin|users.*']);
 Route::post('users/refresh', AuthRefreshTokenController::class)->name('refresh');
 Route::post('users/login', AuthLoginController::class)->name('login');
